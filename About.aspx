7ec5ef0213f2dab4aa7661fd8f721e51285c76c0
alexbollen <alexbollen@umes.edu.gt>
2023-05-18 09:31:41 -0600

    Página para actualizar datos de album

--- About.aspx
+++ About.aspx
@@ -1,7 +1,22 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="LaboratorioNo14.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
-</asp:Content>
+    <h2 align="center">EDICIÓN DE ALBÚM</h2>
+    <h3>Ingrese el nombre del albúm a editar:
+        <asp:TextBox ID="TextBoxBuscar" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="ButtonBuscarAlbum" runat="server" Height="36px" OnClick="ButtonBuscarAlbum_Click" Text="Buscar" Width="99px" />
+&nbsp;<asp:Label ID="LabelAlbum" runat="server" Text="[ALBUM]"></asp:Label>
+    </h3>
+    <p>Titulo del albúm:
+        <asp:TextBox ID="TextBoxTituloAlbum" runat="server"></asp:TextBox>
+    </p>
+    <p>Artista de albúm:
+        <asp:TextBox ID="TextBoxNombreArtista" runat="server"></asp:TextBox>
+    </p>
+    <p>Fecha de publicación:
+        <asp:TextBox ID="TextBoxFechaPublicacion" runat="server"></asp:TextBox>
+    </p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="ButtonActualizar" runat="server" Enabled="False" OnClick="ButtonActualizar_Click" Text="Actualizar" />
+    </p>
+    </asp:Content>
--- About.aspx.cs
+++ About.aspx.cs
@@ -13,5 +13,41 @@ namespace LaboratorioNo14
         {
 
         }
+
+        protected void ButtonBuscarAlbum_Click(object sender, EventArgs e)
+        {
+            Archivo archivo = new Archivo();
+            List<Album> albumes = archivo.Leer();
+            if (albumes != null)
+            {
+                Album albumExiste = albumes.Find(u => u.TituloAlbum == TextBoxBuscar.Text);
+                if (albumExiste != null)
+                {
+                    Response.Write("<script>alert('Album Encontrada.')</script>");
+                    LabelAlbum.Text = albumExiste.TituloAlbum;
+                    TextBoxTituloAlbum.Text = albumExiste.TituloAlbum;
+                    TextBoxNombreArtista.Text = albumExiste.ArtistaAlbum;
+                    TextBoxFechaPublicacion.Text = albumExiste.FechaPublicacion;
+                    ButtonActualizar.Enabled = true;
+                }
+                else 
+                {
+                    Response.Write("<script>alert('Album no existe!')</script>");
+                }
+            }
+        }
+
+        protected void ButtonActualizar_Click(object sender, EventArgs e)
+        {
+            Archivo archivo = new Archivo();
+            List<Album> albumes= new List<Album>();
+            albumes = archivo.Leer();
+            Album album= albumes.Find(a => a.TituloAlbum== LabelAlbum.Text);
+            album.TituloAlbum = TextBoxTituloAlbum.Text;
+            album.ArtistaAlbum = TextBoxNombreArtista.Text;
+            album.FechaPublicacion = TextBoxFechaPublicacion.Text;
+            Response.Write("<script>alert('Album actualizado')</script>");
+            archivo.Grabar(albumes);
+        }
     }
 }
